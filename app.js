2e0ef7e81125ceb359bb743fca039e349c2eb996
xtekkis <xtekkis@uclan.ac.uk>
2026-02-15 19:34:19 +0200

    index.html changes placed the script at the bottom

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-// Function to open burger menu
-function openMenu() {
-    var x = document.getElementById("menu");
-    if (x.className === "menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu";
-    }
-}
 
-// Scroll tracking for animation
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            observer.unobserve(entry.target);
-        }
-    });
-}, {
-    threshold: 0.2
-});
-
-document.querySelectorAll('.fly-in').forEach(el => {
-    observer.observe(el);
-});
 
 //API section
 const API_BASE="https://codecyprus.org/th/api";
